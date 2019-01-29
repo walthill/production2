ec05e6fb25f5c771e1b3effb8267349a6dce9678
Qazik <44536239+Qazik@users.noreply.github.com>
2019-01-28 19:04:22 -0500

    ui on rythm
    adding ui to rythm game

--- prototype2/P2/Assets/MoveLogicScr.cs
+++ prototype2/P2/Assets/MoveLogicScr.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class MoveLogicScr : MonoBehaviour
 {
-
+    public Transform bag;
+    public bool WasScanned = false;
+    public bool WasBagged = false;
+    bool done = false;
     Transform tf;
 
     // Start is called before the first frame update
@@ -18,15 +21,52 @@ public class MoveLogicScr : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.position += Vector3.right * Time.deltaTime;
+        
+        
+        if (WasBagged && WasScanned)
+        {
+            gameObject.transform.position = new Vector3(3.1f, 3.97f, -6.344004f); //* Time.deltaTime;
+            //transform.position = Vector3.Lerp(transform.position)
+            //done = true;
+            addToTimer();
+        }
+
+
+
+        if (!WasBagged){
+            gameObject.transform.position += Vector3.right * Time.deltaTime;
+        }
+        if (done)
+        {
+            //addToTimer();
+        }
+        
     }
 
-    //make this determin where the object is and detect if it has been prossessed.
-    private void OnCollisionEnter(Collision collision)
+    void addToTimer()
     {
-        if (collision.collider.tag == "bagger")
-        {
+        GameStuffScr.instance.addTime();
+        WasScanned = false;
+        GameStuffScr.instance.addScore();
+    }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "scanner")
+        {
+            WasScanned = true;
+            Debug.Log("scanned");
+        }
+        if (other.tag == "bagger")
+        {
+            WasBagged = true;
+            Debug.Log("bagged");
+        }
+        if (other.tag == "Finish")
+        {
+            GameStuffScr.instance.subTimer();
+            GameStuffScr.instance.subScore();
         }
     }
+    
 }
