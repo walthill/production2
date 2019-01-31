dac3ff3b1d6338ac7f96e93bcf7987b9e08d1221
Walter Hill <walterhilldev@gmail.com>
2019-01-31 13:56:43 -0500

    starting work on 3rd person camera
    - added temp player model

---
+++ prototype3/P3/Assets/FollowCamera.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField]
+    GameObject target;
+    [SerializeField]
+    float dampingValue;
+
+    Vector3 offset;
+
+    void Awake()
+    {
+        offset = target.transform.position - gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        float targetAngle, currentAngle, angle;
+
+        currentAngle = gameObject.transform.eulerAngles.y;
+        targetAngle = target.transform.eulerAngles.y;
+        angle = Mathf.LerpAngle(currentAngle, targetAngle, Time.deltaTime * dampingValue);
+
+        Quaternion rotation = Quaternion.Euler(0, targetAngle, 0);
+        gameObject.transform.position = target.transform.position - (rotation * offset);
+
+        transform.LookAt(target.transform);
+    }
+}
