9786103d886e3fbdfd5135f2d60d9a3edb9e4460
walt hill <walterhilldev@gmail.com>
2019-01-31 15:11:08 -0500

    setting out goals & todos. Telly lives!
    - added model for prototype
    - commented out plans for movement, controls, and camera movement
    - added pic of ideal camera location

---
+++ prototype3/P3/Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+
+    /**
+     *  What type of camera do we want? 
+     * 
+     * 
+     * 
+     * */
+
+
+
+    [SerializeField]
+    GameObject target;
+    [SerializeField]
+    float dampingValue;
+
+    Vector3 offset;
+
+    void Awake()
+    {
+        offset = target.transform.position - gameObject.transform.position; //TODO: camera behind
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        float targetAngle, currentAngle, angle;
+
+        currentAngle = gameObject.transform.eulerAngles.y;
+        targetAngle = target.transform.eulerAngles.y;
+        angle = Mathf.LerpAngle(currentAngle, targetAngle, Time.deltaTime * dampingValue);
+
+        Quaternion rotation = Quaternion.Euler(0, targetAngle, 0);
+        gameObject.transform.position = target.transform.position - (rotation * offset);
+
+        transform.LookAt(target.transform);
+    }
+}
---
+++ prototype3/P3/Assets/Scripts/PlayerSkateMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSkateMovement : MonoBehaviour
+{
+
+    /*
+     * CONTROLS
+     * Triggers turning
+     * Up on stick to accel, down to deccel
+     * A to crouch
+     */
+
+    float xMove, zMove, xRotate, yRotate;
+    bool jump, jumpReleased;
+
+    Rigidbody rb;
+
+    void Awake()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        xMove = Input.GetAxis("JoyHorizontal");
+        zMove = Input.GetAxis("JoyVertical");
+        jump = Input.GetButtonDown("JoyJump");
+        jumpReleased = Input.GetButtonUp("JoyJump");
+        yRotate = Input.GetAxis("Rotate Y");
+
+       //TODO: Lerp turning?
+
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3();
+        movement.x = xMove * 5;
+        movement.z = zMove * 5;
+
+
+        rb.velocity = movement; 
+    }
+}
