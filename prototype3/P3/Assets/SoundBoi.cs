e3e0854fd683d20c521ec77da0e79030e68d9bac
Qazik <44536239+Qazik@users.noreply.github.com>
2019-02-06 14:32:15 -0500

    sound
    MADE SOUND MANAGER

---
+++ prototype3/P3/Assets/SoundBoi.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundBoi : MonoBehaviour
+{
+    [Header("General Feedback Sounds")]
+    public AudioClip WheelSoundSlowSnd;
+    public AudioClip WheelSoundFastSnd;
+    public AudioClip SparksSnd;
+    public AudioClip ChargingSnd;
+    public AudioClip ReleaseSnd;
+    public AudioClip PerfectReleaseSnd;
+    [Header("General Feedback Sounds")]
+    public AudioSource WheelSource;
+    public AudioSource sparkSource;
+    public AudioSource chargingSource;
+    public AudioSource ReleaseFeedBackSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
