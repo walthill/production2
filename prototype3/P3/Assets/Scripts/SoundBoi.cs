69c4f90aa2b734acebeff30fc1fc1ad095b42c7b
jo forchheimer <joshua.forchheimer@mymail.champlain.edu>
2019-02-07 20:53:51 -0500

    sound boi key bindings
    sound boi key bindings

--- prototype3/P3/Assets/Scripts/SoundBoi.cs
+++ prototype3/P3/Assets/Scripts/SoundBoi.cs
@@ -13,6 +13,14 @@ public class SoundBoi : MonoBehaviour
     public AudioClip PerfectReleaseSnd;
 
     [Header("Music Sounds")]
+    public AudioClip musicSnd0;
+    public AudioClip musicSnd1;
+    public AudioClip musicSnd2;
+    public AudioClip musicSnd3;
+    public AudioClip musicSnd4;
+    public AudioClip musicSnd5;
+
+    /*
     public AudioClip drumsSnd0;
     public AudioClip drumsSnd1;
     public AudioClip drumsSnd2;
@@ -23,6 +31,7 @@ public class SoundBoi : MonoBehaviour
     public AudioClip melodySnd1;
     public AudioClip arpSnd0;
     public AudioClip arpSnd1;
+    */
 
 
     [Header("General Feedback Sources")]
@@ -32,6 +41,13 @@ public class SoundBoi : MonoBehaviour
     public AudioSource ReleaseFeedBackSource;
 
     [Header("Music Sources")]
+    public AudioSource musicSlot0;
+    public AudioSource musicSlot1;
+    public AudioSource musicSlot2;
+    public AudioSource musicSlot3;
+    public AudioSource musicSlot4;
+    public AudioSource musicSlot5;
+    /*
     public AudioSource drumsSource0;
     public AudioSource drumsSource1;
     public AudioSource drumsSource2;
@@ -42,18 +58,54 @@ public class SoundBoi : MonoBehaviour
     public AudioSource melodySource1;
     public AudioSource arpSource0;
     public AudioSource arpSource1;
-
+    */
+    float heldVolume0;
+    float heldVolume1;
+    float heldVolume2;
+    float heldVolume3;
+    float heldVolume4;
+    float heldVolume5;
 
     // Start is called before the first frame update
     void Start()
     {
 
         
+        heldVolume0 = musicSlot0.volume;
+        heldVolume1 = musicSlot1.volume;
+        heldVolume2 = musicSlot2.volume;
+        heldVolume3 = musicSlot3.volume;
+        heldVolume4 = musicSlot4.volume;
+        heldVolume5 = musicSlot5.volume;
+        SetMusic();
     }
 
-    public void LoadMusic()
+    public void SetMusic()
     {
+
+        
+
+        musicSlot1.volume = 0;
+        musicSlot2.volume = 0;
+        musicSlot3.volume = 0;
+        musicSlot4.volume = 0;
+        musicSlot5.volume = 0;
+
+        musicSlot0.clip = musicSnd0;
+        musicSlot1.clip = musicSnd1;
+        musicSlot2.clip = musicSnd2;
+        musicSlot3.clip = musicSnd3;
+        musicSlot4.clip = musicSnd4;
+        musicSlot5.clip = musicSnd5;
+        musicSlot0.Play();
+        musicSlot1.Play();
+        musicSlot2.Play();
+        musicSlot3.Play();
+        musicSlot4.Play();
+        musicSlot5.Play();
+
         //loads the clips into
+        /*
         drumsSource0.clip = drumsSnd0;
         drumsSource1.clip = drumsSnd1;
         drumsSource2.clip = drumsSnd2;
@@ -64,13 +116,72 @@ public class SoundBoi : MonoBehaviour
         melodySource1.clip = melodySnd1;
         arpSource0.clip = arpSnd0;
         arpSource1.clip = arpSnd1;
+        */
+
     }
 
+
+
+
+
+    public void LoadGeneralSounds()
+    {
+        //loads the clips into
+        WheelSource.clip = WheelSoundSlowSnd;
+        sparkSource.clip = SparksSnd;
+        chargingSource.clip = ChargingSnd;
+        ReleaseFeedBackSource.clip = ReleaseSnd;
+    }
 
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            VolumeMusic1();
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            VolumeMusic2();
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            VolumeMusic3();
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            VolumeMusic4();
+        }
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            VolumeMusic5();
+        }
     }
+
+    //these raise the volume of the sounds as speed inceases
+    //they will be called from a different script in the final version
+    //20:00 2/7/19
+    //right now they are triggered from update
+    public void VolumeMusic1()
+    {
+        musicSlot1.volume = heldVolume1;
+    }
+    public void VolumeMusic2()
+    {
+        musicSlot2.volume = heldVolume2;
+    }
+    public void VolumeMusic3()
+    {
+        musicSlot3.volume = heldVolume3;
+    }
+    public void VolumeMusic4()
+    {
+        musicSlot4.volume = heldVolume4;
+    }
+    public void VolumeMusic5()
+    {
+        musicSlot5.volume = heldVolume5;
+    }
+
 }
