a672bffe33eba94a73ecc6234c05fbaadbbfeb91
jo forchheimer <joshua.forchheimer@mymail.champlain.edu>
2019-02-13 17:40:52 -0500

    boombox lerp kinda
    boombox lerp kinda

--- prototype3/P3/Assets/TempLerpHardCode.cs
+++ prototype3/P3/Assets/TempLerpHardCode.cs
@@ -7,7 +7,7 @@ public class TempLerpHardCode : MonoBehaviour
     bool keepMoving = false;
     public Transform target;
     public Transform BoomBox;
-    public float LerpSpeed = 1;
+    public float LerpSpeed = .5f;
     // Time when the movement started.
     private float startTime;
     // Total distance between the markers.
@@ -26,6 +26,27 @@ public class TempLerpHardCode : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (keepMoving)
+        {
+            // Distance moved = time * speed.
+            float distCovered = (Time.time - startTime) * LerpSpeed;
+
+            // Fraction of journey completed = current distance divided by total distance.
+            float fracJourney = distCovered / journeyLength;
+
+            // Set our position as a fraction of the distance between the markers.
+            transform.position = Vector3.Lerp(BoomBox.position, target.position, fracJourney);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "boomBox"){
+            keepMoving = true;
+        }
+        if (other.tag == "end")
+        {
+            keepMoving = false;
+        }
     }
 }
