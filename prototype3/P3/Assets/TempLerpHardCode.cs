1331f160a6e7223bddcd04e75f00c93526f3d358
jo forchheimer <joshua.forchheimer@mymail.champlain.edu>
2019-02-13 17:23:32 -0500

    boombox jump
    boom box jump (not yet working but getting there)

---
+++ prototype3/P3/Assets/TempLerpHardCode.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempLerpHardCode : MonoBehaviour
+{
+    bool keepMoving = false;
+    public Transform target;
+    public Transform BoomBox;
+    public float LerpSpeed = 1;
+    // Time when the movement started.
+    private float startTime;
+    // Total distance between the markers.
+    private float journeyLength;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Keep a note of the time the movement started.
+        startTime = Time.time;
+
+        // Calculate the journey length.
+        journeyLength = Vector3.Distance(target.position, BoomBox.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
