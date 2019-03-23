0e4ef1196b1350dde52a9bf77b585d426236970d
Jacob <jacob.biederman@mymail.champlain.edu>
2019-03-23 13:24:04 -0400

    Started on speed thresholds

---
+++ prototype3/P3/Assets/Scripts/SpeedThresholdBoi.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SpeedChannel { QUICK, SPEEDY, FAST, BLUR, LIGHTNING, WOW_SO_FAST, NUM_SPEEDS }
+
+public class SpeedThresholdBoi : MonoBehaviour
+{
+    //base speed = 50
+    [SerializeField]
+    [Tooltip("Speed thresholds")]
+    int[] speeds = new int[(int)SpeedChannel.NUM_SPEEDS];
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
