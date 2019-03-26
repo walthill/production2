fee8243f1a1c01b7c48ac7cf085b5918af1eb790
Ben Strong <benjamin.strong@mymail.champlain.edu>
2019-03-26 19:48:53 -0400

    Made Collectable. No placements or linking to sound yet.

---
+++ prototype3/P3/Assets/Scripts/Collectable.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    [Range(0.1f, 1.0f)]
+    public float bounceHeight;
+    [Range(0.1f, 2.0f)]
+    public float bounceSpeed;
+    [Range(10.0f, 20.0f)]
+    public float rotationSpeed;
+
+    float startY;
+
+    void Start()
+    {
+        startY = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rotate();
+        Bounce();
+    }
+
+    void Rotate()
+    {
+        transform.Rotate(0, 0, Time.deltaTime * rotationSpeed);
+    }
+
+    void Bounce()
+    {
+        float bounce = bounceHeight * Mathf.Sin(Time.time * bounceSpeed);
+        transform.position = new Vector3(transform.position.x, startY + bounce , transform.position.z);
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        //MAKE LINK TO SOUND AND/OR PLAYER HERE
+        //
+        //
+        //
+        //
+        //MAKE LINK TO SOUND AND/OR PLAYER HERE
+    }
+}
