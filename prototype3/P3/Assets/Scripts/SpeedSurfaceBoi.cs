3d03d3e7d0469a59f671670a4d7f730dbfde63e4
Jacob <jacob.biederman@mymail.champlain.edu>
2019-04-06 12:14:37 -0400

    Added speedSurfaceBoi and scripts on speed surfaces

---
+++ prototype3/P3/Assets/Scripts/SpeedSurfaceBoi.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedSurfaceBoi : MonoBehaviour
+{
+    SpeedSurfaceScript[] speedSurfaces;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speedSurfaces = GameObject.FindObjectsOfType<SpeedSurfaceScript>();
+        Debug.Log(speedSurfaces.Length);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
