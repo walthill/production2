d3f4ba07fd760de6c2956df6f6a6f5acfd6de007
Unknown <leo.robles.gonzalez99@gmail.com>
2019-04-20 20:56:20 -0400

    Made functional speed surface UI
    But it ain't pretty.

---
+++ prototype3/P3/Assets/Scripts/UISpeedSurfaceBar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UISpeedSurfaceBar : MonoBehaviour
+{
+    float shortSide = 0.1f;
+    float longside = 0.27f;
+
+    public GameObject[] bars;
+
+    public void SetCurrentBar(int activeBar)
+    {
+        if (activeBar < bars.Length)
+        {
+            bars[activeBar].GetComponent<RectTransform>().sizeDelta = new Vector2(shortSide, longside);
+        }
+    }
+
+    public void ResetBars()
+    {
+        foreach (GameObject bar in bars)
+        {
+            bar.GetComponent<RectTransform>().sizeDelta = new Vector2(shortSide, shortSide);
+        }
+    }
+}
