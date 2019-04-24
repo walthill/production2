b897c7d5193020ff8bf336b60c5515f57ed52826
Leo Robles <leo.robles.gonzalez99@gmail.com>
2019-04-23 23:24:38 -0400

    How To Play screen is fully implemented
    Builds MUST be run at 1920x1080!

---
+++ prototype3/P3/Assets/Scripts/InformationHolder.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InformationHolder : MonoBehaviour
+{
+    public GameObject relatedInfo;
+    public GameObject whiteNoise;
+    bool canChange = false;
+
+    private void Update()
+    {
+        if (EventSystem.current.currentSelectedGameObject != this.gameObject)
+        {
+            relatedInfo.SetActive(false);
+            canChange = true;
+        }
+        else
+        {
+            if (canChange && gameObject.name != "0. BACK TO MENU")
+            {
+                whiteNoise.gameObject.transform.Find("RawImage/WhiteNoise").GetComponent<WhiteNoiseScript>().animCycles = 0;
+                whiteNoise.SetActive(true);
+                relatedInfo.SetActive(true);
+                canChange = false;
+            }
+        }
+    }
+}
